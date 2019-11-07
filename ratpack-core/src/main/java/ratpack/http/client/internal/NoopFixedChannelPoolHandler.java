6e6005fec48d64f1813da9cfe4b08712f5a38dd2
DanMaas <daniel.maas@target.com>
2019-11-07 14:55:38 -0600

    adding client proxy capability
    feature: Implement connection proxy at the channel layer.

--- ratpack-core/src/main/java/ratpack/http/client/internal/NoopFixedChannelPoolHandler.java
+++ ratpack-core/src/main/java/ratpack/http/client/internal/NoopFixedChannelPoolHandler.java
@@ -18,8 +18,11 @@ package ratpack.http.client.internal;
 
 import io.netty.channel.Channel;
 import io.netty.channel.pool.AbstractChannelPoolHandler;
+import io.netty.handler.proxy.HttpProxyHandler;
 import io.netty.handler.timeout.IdleStateHandler;
 
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
 import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
@@ -29,21 +32,26 @@ public class NoopFixedChannelPoolHandler extends AbstractChannelPoolHandler impl
 
   private final String host;
   private final Duration idleTimeout;
+  private final ProxyInternal proxy;
 
-  public NoopFixedChannelPoolHandler(HttpChannelKey channelKey, Duration idleTimeout) {
+  public NoopFixedChannelPoolHandler(HttpChannelKey channelKey, Duration idleTimeout, ProxyInternal proxy) {
     this.host = channelKey.host;
     this.idleTimeout = idleTimeout;
+    this.proxy = proxy;
   }
 
   @Override
   public void channelCreated(Channel ch) throws Exception {
+    if  (proxy != null && proxy.shouldProxy(host)) {
+      SocketAddress proxyAddress = new InetSocketAddress(proxy.getHost(), proxy.getPort());
+      ch.pipeline().addLast(new HttpProxyHandler(proxyAddress));
+    }
   }
 
   @Override
   public void channelReleased(Channel ch) throws Exception {
     if (ch.isOpen()) {
       ch.config().setAutoRead(true);
-      ch.pipeline().addLast(IdlingConnectionHandler.INSTANCE);
       if (idleTimeout.toNanos() > 0) {
         ch.pipeline().addLast(IDLE_STATE_HANDLER_NAME, new IdleStateHandler(idleTimeout.toNanos(), idleTimeout.toNanos(), 0, TimeUnit.NANOSECONDS));
         ch.pipeline().addLast(IdleTimeoutHandler.INSTANCE);
